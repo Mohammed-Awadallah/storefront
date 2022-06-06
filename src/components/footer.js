b1cf7e0a92023d893c921f4fc85c62e2cd7795f7
Mohammed-Awadallah <dr.mohammed.j.awadallah@gmail.com>
2022-06-07 02:28:23 +0300

    lab36 finished

---
+++ src/components/footer.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+ 
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.zeta,
+    marginTop: theme.spacing.unit * 8,
+    padding: `${theme.spacing.unit * 6}px 0`,
+  }
+});
+ 
+function Footer(props) {
+  const { classes } = props;
+ 
+  return (
+    <footer className={classes.footer}>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="h5" component="h3">
+         Mohammed Awadallah
+        </Typography>
+        <Typography component="p">
+          @2022 ASAC
+        </Typography>
+      </Paper>
+    </footer>
+  );
+}
+ 
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+ 
+export default withStyles(styles)(Footer);
---
+++ src/store/categories.js
@@ -0,0 +1,23 @@
+const initialState = {
+    allCategories: {
+        food: {name:'Food', displayName: 'food', description: 'all food related products', image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+        electronics: {name:'Electronics', displayName: 'electronics', description: 'all electronics'},
+        makeup: {name:'Makeup', displayName: 'makeup', description: 'All makeup related products'},
+        kitchen: {name:'Kitchen', displayName: 'kitchen', description: 'all kitchen related products'},
+    },
+      activeCategory: {name:'choose-category', displayName: 'Catagories', description: 'choose a category to see what we have available'},
+  }
+  
+  
+  // Reducer
+  export default (state = initialState, action) => {
+    const {type, payload} = action;
+    switch(type) {
+      case 'Change':
+        console.log(payload, state.allCategories[payload]);
+        return {...state, activeCategory: state.allCategories[payload]};
+  
+      default:
+        return state;
+    }
+  }
